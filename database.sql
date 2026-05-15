6e7aac77c0280be5a77ed9f32248b5eee8d6c9ab
uchiyamaairi <nkc20236174@st.denpa.jp>
2026-05-15 12:09:04 +0900

    データベース作成

--- database.sql
+++ database.sql
@@ -4,34 +4,34 @@ USE vission_asset;
 
 CREATE TABLE Facilities (
     id BINARY(16) PRIMARY KEY,
-    name VARCHAR(10) NOT NULL,
-    address VARCHAR(255),
-    representative_name VARCHAR(100),
+    name VARCHAR(20) NOT NULL,
+    address VARCHAR(30),
+    representative_name VARCHAR(20),
     emergency_contact VARCHAR(11)
 );
 
 CREATE TABLE Admins (
     id BINARY(16) PRIMARY KEY,
     facility_id BINARY(16) FOREIGN KEY,
-    login_id VARCHAR(100),
+    login_id VARCHAR(10)NOT NULL,
     password_hash VARCHAR(255),
-    name VARCHAR(10)
+    name VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE Patients (
     id BINARY(16) PRIMARY KEY,
     facility_id BINARY(16) FOREIGN KEY,
     name VARCHAR(10) NOT NULL,
-    age CHAR(3),
+    age INT(3),
     emergency_note VARCHAR(100),
-    login_id VARCHAR(100),
+    login_id VARCHAR(10)NOT NULL,
     password_hash VARCHAR(255)
 );
 
 CREATE TABLE Devices (
     id BINARY(16) PRIMARY KEY,
     patient_id BINARY(16) FOREIGN KEY,
-    device_mac_address VARCHAR(255),
+    device_mac_address VARCHAR(17),
     battery_status CHAR(3),
     last_hertbeat DATETIME
 );
@@ -44,7 +44,7 @@ CREATE TABLE WalkingLogs (
 );
 
 CREATE TABLE CPSData (
-    id BIGINT(100) PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     log_id BINARY(16) FOREIGN KEY,
     latitude DECIMAL(7,4),
     longitude DECIMAL(7,4),
