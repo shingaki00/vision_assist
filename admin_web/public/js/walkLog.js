10c01b9448351644f560b181a7c093bf8c48759d
{CTFukutomi} <{gakkougurasi2525@gmail.com}>
2026-05-26 14:53:53 +0900

    矢印部分のフォーマットを統一させた

--- admin_web/public/js/walkLog.js
+++ admin_web/public/js/walkLog.js
@@ -155,6 +155,9 @@ function renderList() {
       </div>
     
       <!-- ここで＞を入れる -->
+      <svg class="chevron" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+        <polyline points="9 18 15 12 9 6"/>
+      </svg>
     
     </div>
   `).join("");
