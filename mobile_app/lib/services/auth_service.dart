096e4ce60510434865c214ddf94b2033a33a9831
uchiyamaairi <nkc20236174@st.denpa.jp>
2026-07-03 12:27:38 +0900

    ユーザログイン作成

--- mobile_app/lib/services/auth_service.dart
+++ mobile_app/lib/services/auth_service.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 /// バックエンド担当者と連携するための認証サービス
 class AuthService {
@@ -7,16 +9,29 @@ class AuthService {
 
   /// ログイン処理
   /// 現在はテスト用。将来的に http パッケージを使用してAPIと通信します。
-  Future<bool> login(String id, String password) async {
+  Future<bool> login(String mailAddress, String password) async {
     // --- 通信のシミュレーション ---
     await Future.delayed(const Duration(seconds: 1));
 
     // --- テスト用ログイン条件 ---
     // 他の人が設計したDBの初期ユーザーに合わせて変更してください
-    if (id == 'admin' && password == 'pass') {
-      return true;
+    // if (id == 'admin' && password == 'pass') {
+    //   return true;
+    // }
+
+    final response = await http.post(Uri.parse("http://localhost:3000/login"),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: jsonEncode({
+          "mail_address": mailAddress,
+          "password": password,
+        }));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data["success"];
     }
-    
+
     // --- API連携時のイメージ ---
     /*
     final response = await http.post(
@@ -25,7 +40,8 @@ class AuthService {
     );
     return response.statusCode == 200;
     */
-    
+    //登録されたユーザのログイン処理実装する
+
     return false;
   }
 }
